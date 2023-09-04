7fc52fa2d1286c689b99636cd3939eef93226296
Admin <prathik.bht@gmail.com>
2023-09-04 19:09:35 +0530

    New class file Samplenew

---
+++ helloworldnew1.java
@@ -0,0 +1,7 @@
+class Samplenew{
+    public static void main(String args[]){
+ System.out.println("Hello Students from Batch 31");
+    System.out.println("I am so happy to be learning DevOps");
+            System.out.println("I fixed my first bug with the help of chatgpt and it rightly said compiler was not running");
+}
+}
